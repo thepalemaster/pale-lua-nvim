51906187cd4bb77a94f08ae8461b833fc00bc1ae
thepalemaster <thepalemaster@gmail.com>
2023-02-07 21:36:15 +0500

    v2

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -127,5 +127,16 @@ return require('packer').startup(function()
     use 'hrsh7th/cmp-path'                              
     use 'hrsh7th/cmp-buffer'                            
     use 'hrsh7th/vim-vsnip'   
+    
+    --файловая система вызвать :Neotree
+	use {
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		requires = { 
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		}
+	}
 	
 end)
